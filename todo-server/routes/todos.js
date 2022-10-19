a768463ed4c8d2c48e8da8ee929cd5e9bb90be9d
cr504 <chrisrivasdev@gmail.com>
2022-10-19 09:22:33 -0400

    Modified error msg

--- todo-server/routes/todos.js
+++ todo-server/routes/todos.js
@@ -6,11 +6,12 @@ const TodosController = require('../controllers/todos');
 router.get('/todos', (req, res) => {
 try {    
     const todosController = new TodosController();      
-    const allTodos = todosController.getAllTodos();    
+    const allTodos = todosController.getAllTodos();        
     return res.status(200).send(allTodos);    
 } catch (error) {
-    console.log(`server: GET todos failed ${error}`);
-    res.status(500).send();
+    const errorMsg = `server: GET todos failed ${error}`
+    console.log(errorMsg);
+    res.status(500).send(errorMsg);
 }
 });
 
