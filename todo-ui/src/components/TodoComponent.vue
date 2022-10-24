d7936922bb6be603384df1166f07dc0c47075a46
cr504 <chrisrivasdev@gmail.com>
2022-10-23 20:41:16 -0400

    Added ability to add and delete todos

--- todo-ui/src/components/TodoComponent.vue
+++ todo-ui/src/components/TodoComponent.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    <h3>Todo App</h3>
-    <ul v-if="todos">
-      <li v-for="todo of todos">
-        <p><strong>{{todo.title}}</strong></p>        
-      </li>
-    </ul>
+<div>
+    <v-text-field
+    v-model="newTodoTitle"
+    @click:append="addTodo"
+    @keyup.enter="addTodo"
+    class="pa-3" outlined label="Add Todo" 
+    append-icon="mdi-plus-thick" hide-details clearable></v-text-field>
+    <v-list v-if="todos.length" class="pt-0" flat>
+      <div v-for="todo in todos" :key="todo.id">
+        <v-list-item>
+          <template v-slot:default>     
+            
+            <v-list-item-content>
+              <v-list-item-title>{{todo.title}}</v-list-item-title>
+            </v-list-item-content>            
+
+            <v-list-item-action>
+              <v-btn @click.stop="deleteTodo(todo.id)" icon>
+                <v-icon color="red">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else
+    class="no-todos">
+      <v-icon      
+      color="primary"
+      size="100"
+    >
+      mdi-check
+    </v-icon>
+      <div class="text-h5 primary--text">No todos</div>
+    </div>
   </div>
 </template>
 
@@ -15,12 +45,14 @@ import axios from 'axios';
   export default {    
     data() {
       return {
+        newTodoTitle: '', 
         todos: [],
         errors: []      
     }
   },
 
   created() {
+    // Get all todos on created
     axios.get(`http://localhost:3000/todos`)
     .then(response => {
       this.todos = response.data
@@ -28,6 +60,30 @@ import axios from 'axios';
     .catch(e => {
       this.errors.push(e)
     })    
+  },
+
+  methods: {    
+    addTodo() {
+      let newTodo = {
+        id: this.todos.length + 1,
+        title: this.newTodoTitle,        
+      }
+      this.todos.push(newTodo)
+      this.newTodoTitle = ''
+    },
+
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
   }
 }
 </script>
+
+<style lang="sass">
+.no-todos
+  position: absolute
+  left: 50%
+  top: 50%
+  transform: translate(-50%, -50%)
+  opacity: 50
+</style>
