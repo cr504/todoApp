c56eae187a72281d1b300ecb9e48bfa995907367
cr504 <chrisrivasdev@gmail.com>
2022-10-23 20:02:21 -0400

    setup axios request

--- todo-ui/src/components/TodoComponent.vue
+++ todo-ui/src/components/TodoComponent.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h3>Todo App</h3>
-    
+    <ul v-if="todos">
+      <li v-for="todo of todos">
+        <p><strong>{{todo.title}}</strong></p>        
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,9 +13,21 @@
 import axios from 'axios';
 
   export default {    
+    data() {
+      return {
+        todos: [],
+        errors: []      
+    }
+  },
 
-    data: () => ({
-      
-    }),
+  created() {
+    axios.get(`http://localhost:3000/todos`)
+    .then(response => {
+      this.todos = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })    
   }
+}
 </script>
